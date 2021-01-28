0b7546f13871ac768b1d704d8bfa6ce3d2aac5df
asters1 <GJ744559140@163.com>
2021-01-28 19:29:45 +0800

    mod

--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -187,6 +187,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod4Mask
+#define MODKEY1 Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
@@ -203,6 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY1,               XK_Page_Up,     kscrollup,      {.i = 1} },
+	{ MODKEY1,               XK_Page_Down,   kscrolldown,    {.i = 1} },
 	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
